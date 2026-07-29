9ae0d923356abdf1c4fd070919458be0030b9934
anapearl06 <shuklaananya762@gmail.com>
2026-07-30 00:22:24 +0530

    js

---
+++ JS FOUNDATION/part1/datatypes.js
@@ -0,0 +1,31 @@
+console.log("Hello");
+// console.log("chaii");
+/*
+
+String
+Number
+Boolean
+Bigint
+
+Undefined
+null (empty)
+
+Object
+
+Symbol
+
+*/
+
+// old way  var score = 102
+
+let score = 102;
+let name = "ananya.com"
+let isLoggedinm= false
+
+//object
+let teaTypes = ["Lemon Tea", "Orange Tea", "oolong Tea"]
+let user = {firstname: "Ananya", lastname: "Shukla"}
+
+let getScore = Score;
+
+console.log(getScore);
