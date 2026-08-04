8493adab713421f7d4e4013c8bce1e52a18952b0
anapearl06 <shuklaananya762@gmail.com>
2026-08-04 13:11:52 +0530

    adv

--- JS FOUNDATION/part9_adv/appC.js
+++ JS FOUNDATION/part9_adv/appC.js
@@ -1,3 +1,9 @@
 const mathOperations = require("./mathOperationsC.js");
 
 console.log(mathOperations.add(2, 2));
+
+const multiply = require("./mathOperationsC.js").default;
+
+console.log(multiply(2, 2));
+
+
