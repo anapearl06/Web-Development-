48bd4c5049d999fa3be2086750dd893707552b51
anapearl06 <shuklaananya762@gmail.com>
2026-08-12 18:26:45 +0530

    js

--- NODE.JS/PRO_MANAGEMENT/index.js
+++ NODE.JS/PRO_MANAGEMENT/index.js
@@ -1,1 +1,8 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+let myusername = process.env.USERNAME;
+
+console.log("Value of myusername:", myusername);
 console.log("Starting the Backend Project Management Application");
