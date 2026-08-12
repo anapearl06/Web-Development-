4625608346e3ede674ee425fc56d22d341d92f62
anapearl06 <shuklaananya762@gmail.com>
2026-08-12 19:02:48 +0530

    js

--- NODE.JS/PRO_MANAGEMENT/src/index.js
+++ NODE.JS/PRO_MANAGEMENT/src/index.js
@@ -1,8 +1,22 @@
+import express from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-let myusername = process.env.USERNAME;
+const app = express();
+const port = process.env.PORT || 3000;
 
-console.log("Value of myusername:", myusername);
-console.log("Starting the Backend Project Management Application");
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port https://localhost:${port}`);
+    console.log("Starting the Backend Project Management Application");
+    console.log("Value of myusername:", process.env.USERNAME);
+});
+
+// let myusername = process.env.USERNAME;
+
+// console.log("Value of myusername:", myusername);
+//console.log("Starting the Backend Project Management Application");
