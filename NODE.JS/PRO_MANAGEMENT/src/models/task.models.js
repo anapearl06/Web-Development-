9268e0a71dd85535fc87a3b5858ce086e31378b5
anapearl06 <shuklaananya762@gmail.com>
2026-09-16 03:41:42 +0530

    Added

--- NODE.JS/PRO_MANAGEMENT/src/models/task.models.js
+++ NODE.JS/PRO_MANAGEMENT/src/models/task.models.js
@@ -14,15 +14,6 @@ const taskSchema = new Schema(
       ref: "Project",
       required: true,
     },
-    assignedTo: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    },
-    assignedBy: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    },
-    status: {
       type: String,
       enum: AvailableTaskStatues,
       default: TaskStatusEnum.TODO,
