a821488a9da10cff115fa99f1ee6dde46c38f64f
anapearl06 <shuklaananya762@gmail.com>
2026-09-16 03:41:34 +0530

    Added

--- NODE.JS/PRO_MANAGEMENT/src/models/task.models.js
+++ NODE.JS/PRO_MANAGEMENT/src/models/task.models.js
@@ -8,7 +8,11 @@ const taskSchema = new Schema(
       required: true,
       trim: true,
     },
-
+    description: String,
+    project: {
+      type: Schema.Types.ObjectId,
+      ref: "Project",
+      required: true,
     },
     assignedTo: {
       type: Schema.Types.ObjectId,
@@ -38,4 +42,3 @@ const taskSchema = new Schema(
 );
 
 export const Taks = mongoose.model("Task", taskSchema);
-
