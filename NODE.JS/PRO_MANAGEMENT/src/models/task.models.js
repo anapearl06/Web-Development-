3cf27db5bd170384709bc2ad31f91073af04dd47
anapearl06 <shuklaananya762@gmail.com>
2026-09-16 03:41:00 +0530

    Added

--- NODE.JS/PRO_MANAGEMENT/src/models/task.models.js
+++ NODE.JS/PRO_MANAGEMENT/src/models/task.models.js
@@ -8,11 +8,7 @@ const taskSchema = new Schema(
       required: true,
       trim: true,
     },
-    description: String,
-    project: {
-      type: Schema.Types.ObjectId,
-      ref: "Project",
-      required: true,
+
     },
     assignedTo: {
       type: Schema.Types.ObjectId,
