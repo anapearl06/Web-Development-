c7711fa804274f11d0a58c8ec3a85119876453aa
anapearl06 <shuklaananya762@gmail.com>
2026-09-16 20:00:00 +0530

    Added

--- NODE.JS/PRO_MANAGEMENT/src/models/user.models.js
+++ NODE.JS/PRO_MANAGEMENT/src/models/user.models.js
@@ -27,7 +27,7 @@ const userSchema = new Schema(
       type: String,
       required: true,
       unique: true,
-      lowercase: true,
+      lowercase: tru,
       trim: true,
     },
     fullName: {
